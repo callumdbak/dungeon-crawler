3b2d9782ab2efe321a13c45bda1a5789b6fef285
callumdbak <callumdbak@gmail.com>
2026-03-30 09:43:36 -0700

    made changes in class

--- Map.java
+++ Map.java
@@ -4,28 +4,48 @@ public class Map {
    
    public int getNumRows() {
       // TODO: return the number of rows in roomGrid
+      return roomGrid.length;
    }
    public int getNumCols() {
       // TODO: return the number of columns in roomGrid
+      return roomGrid[0].length;
    
    }
 
    public Map(int rows, int columns) {
       // TODO: initalize the Map by creating the 2D array
       // and then calling populateMap which will build all of the Rooms
+      this.roomGrid = new Room[rows][columns];
+      populateMap();
    }
    public void populateMap() {
       // TODO: populate the map with a bunch of Rooms with random numbers of monsters and items
       // Hint: Use nested for loops to fill every row and column
       // Another Hint: use the Room constructor to populate the Monsters and Items
+      
+      for (int i = 0; i > roomGrid[0].length; i++)
+      {
+         for (int g = 0; g > roomGrid.length; g++)
+         {
+            roomGrid[i][g] = new Room();
+         }
+      }
+      
+      
    }
    public Room getRoom(int row, int column) {
       // TODO: return the Room that is at the row and column that are passed
+      return roomGrid[row][column];
    }
    
    public Room getRandomRoom() {
       // TODO: This method will simply return a random room in the Map so that you can 
       // give the player a random starting point.
+      
+      int randomRow = (int) (Math.random() * getNumRows());
+      int randomCol = (int) (Math.random() * getNumCols());
+      
+      return roomGrid[randomRow][randomCol];
       
    }
 }
